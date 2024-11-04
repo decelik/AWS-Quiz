fd7133775e881cdba932c3c855e633c28effe977
Deniz Celik <deniz-dev@celik-isik.de>
2024-11-04 09:10:25 +0100

    Create questions.js
    added questions.js

---
+++ questions.js
@@ -0,0 +1,35 @@
+export const questions = [
+  {
+    question: "",
+    answers: [
+      { text: "", correct: false },
+      { text: "", correct: false },
+      { text: "", correct: false },
+      { text: "", correct: false },
+    ],
+    explanation:
+      "",
+  },
+{
+    question: "",
+    answers: [
+      { text: "", correct: false },
+      { text: "", correct: false },
+      { text: "", correct: false },
+      { text: "", correct: false },
+    ],
+    explanation:
+      "",
+  },
+{
+    question: "",
+    answers: [
+      { text: "", correct: false },
+      { text: "", correct: false },
+      { text: "", correct: false },
+      { text: "", correct: false },
+    ],
+    explanation:
+      "",
+  },
+];
